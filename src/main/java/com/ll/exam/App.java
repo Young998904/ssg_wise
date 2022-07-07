ec9ae485ebdde78ad0ef69eb12157da47a674c3b
Young998904 <like0998904@gmail.com>
2022-07-07 15:01:38 +0900

    명령 등록 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public void run() {
+        int count = 1;
         System.out.println("=== 명언 SSG ===");
         Scanner sc = new Scanner(System.in);
 
@@ -15,6 +16,13 @@ public class App {
             switch (cmd) {
                 case "종료" :
                     break outer;
+                case "등록" :
+                    System.out.printf("명언 : ");
+                    String wise_saying = sc.nextLine();
+                    System.out.printf("작가 : ");
+                    String who = sc.nextLine();
+                    System.out.printf("%d번 명언이 등록되었습니다. \n", count);
+                    count ++;
             }
         }
         sc.close();
