42bc70883a17e3dc70b076c1e9fa1d5e20f1a5e5
Young998904 <like0998904@gmail.com>
2022-07-07 15:42:35 +0900

    등록 명령어 수정 : trim(), break 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -18,11 +18,12 @@ public class App {
                     break outer;
                 case "등록" :
                     System.out.printf("명언 : ");
-                    String wise_saying = sc.nextLine();
+                    String wise_saying = sc.nextLine().trim();
                     System.out.printf("작가 : ");
-                    String who = sc.nextLine();
+                    String who = sc.nextLine().trim();
                     System.out.printf("%d번 명언이 등록되었습니다. \n", count);
                     count ++;
+                    break; // break 하지 않으면 하위 case 들도 수행됨
             }
         }
         sc.close();
